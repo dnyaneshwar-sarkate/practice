c2f08baba39347aec42a64f36acae7a9fc4473a5
dnyaneshwar <dks.personal9313@gmail.com>
2026-06-13 00:43:22 +0530

    modified find_1st_non-repeating_char.py

--- find_1st_non-repeating_char.py
+++ find_1st_non-repeating_char.py
@@ -5,9 +5,21 @@ Ex. s = "aabbccde"
 -> d
 '''
 
-text1 = 'aabbccde'
+# text1 = 'aabbccde'
 
-for i in text1:
-    if text1.count(i) == 1:
-        print(i)
-        break
+# def find_1st_non_repeating_char(text1):
+#     for i in text1:
+#         if text1.count(i) == 1:
+#             return i
+#             break
+#     else:
+#         return 'Not Found!'
+    
+# print(find_1st_non_repeating_char(text1))
+
+
+##########################################################################################################################################################
+# Using List Comprehension 
+
+# res = [i for i in text1 if text1.count(i) == 1]
+# print(res[0])
